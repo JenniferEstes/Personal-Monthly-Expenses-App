95a5b324368b94a8f0f42ada723cacbdf473a005
JenniferEstes <78186496+JenniferEstes@users.noreply.github.com>
2021-03-22 19:03:39 -0500

    Ran Corneal gem to start file structure

---
+++ personal-monthly-expenses-app/config/environment.rb
@@ -0,0 +1,14 @@
+#take environment var Sinatra, if we have it, and if it hasn't been declared, it's falsey and it's value will set to the str developement
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+#connection to db
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require './app/controllers/application_controller'
+require_all 'app'
