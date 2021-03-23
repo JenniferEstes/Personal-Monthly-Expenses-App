a85504f3f8a50d334131a96e8f375e184e5b9cc1
Jennifer <jenndestes@gmail.com>
2021-03-22 21:43:57 -0500

    Set up database.yml file to maintain compatability with activerecord if version changes are made, as well as, created views files

--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
 require_relative './config/environment'
 
+use Rack::MethodOverride
+use Rack::Session::Cookie
+use SessionsController
+
 run ApplicationController
--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 #connection to db
-ActiveRecord::Base.establish_connection(
+ActiveRecord::Base.establish_connection({
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+                                        })
 
 require './app/controllers/application_controller'
 require_all 'app'
