19447ea896e68a464899284e3e1c791d56f58fea
Jennifer <jenndestes@gmail.com>
2021-03-23 19:00:21 -0500

    Migrated users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_041520) do
+ActiveRecord::Schema.define(version: 2021_03_23_235438) do
 
   create_table "expenses", force: :cascade do |t|
     t.datetime "date"
     t.string "description"
     t.float "amount"
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
