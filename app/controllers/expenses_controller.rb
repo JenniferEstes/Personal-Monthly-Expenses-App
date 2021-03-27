6e3bf5e35cf5aac5e1ae07355437e2094e2ed6b9
Jennifer <jenndestes@gmail.com>
2021-03-26 23:24:04 -0500

    Cleaned up views, deleted comments I don't need anymore, and corrected edit route

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,62 +1,54 @@
-require 'pry'
 class ExpensesController < ApplicationController
   #create (render form)
   get '/expenses/new' do
     erb :'expenses/new'
   end
+
   #create (save in db)
   post '/expenses' do
-    binding.pry
     @expense = Expense.create(params["expense"])
     redirect "expenses/#{@expense.id}"
   end
-  #create (save in db)
-  # post '/expenses' do
-  #   expense = Expense.new (params["expense"])
-  #   if expense.save
-  #     redirect '/expenses/#{expense.id}'
-  #   else
-  #     redirect '/expenses/new'
-  #   end
-  # end
+
   #read all
   get '/expenses' do
     @expenses = Expense.all
     erb :'expenses/index'
   end
+
   #read one
   get '/expenses/:id' do
-    @expense = Expense.find_by_id(params[:id])
+    @expense = Expense.find_by(id: params[:id])
     erb :'expenses/show'
   end
-  #update (render form)
-  get 'expenses/:id/edit' do
-    @expense = Expense.find_by_id(params[:id])
+
+  # update (render form)
+  get '/expenses/:id/edit' do
+    @expense = Expense.find_by(id: params[:id])
     erb :'expenses/edit'
   end
+
   #update (save in db)
   patch '/expenses/:id' do
-    expense = Expense.find_by_id(params[:id])
-    if expense.update(params["expense"])
-      redirect "/expenses/#{@expense.id}"
-    else
-      redirect "/expenses/#{@expense.id}/edit"
-    end
+    @expense = Expense.find_by(id: params[:id])
+    @expense.update(params["expense"])
+    redirect "/expenses/#{@expense.id}"
   end
-  #delete expense
-  delete '/expenses/:id' do
-    expense = Expense.find_by_id(params[:id])
-    expense.destroy
 
+  #delete
+  delete '/expenses/:id' do
+    @expense = Expense.find_by(id: params[:id])
+    @expense.destroy
     redirect '/expenses'
   end
-  # delete expense (save in db)
-  patch '/expenses/:id' do
-    expense = Expense.find_by_id(params[:id])
-    if expense.update(params["expense"])
-      redirect "/expenses/#{@expense.id}"
-    else
-      redirect "/expense/#{@expense.id}/edit"
-    end
-  end
+
+  # # delete expense (save in db)
+  # patch '/expenses/:id' do
+  #   @expense = Expense.find_by_id(params[:id])
+  #   if expense.update(params["expense"])
+  #     redirect "/expenses/#{@expense.id}"
+  #   else
+  #     redirect "/expense/#{@expense.id}/edit"
+  #   end
+  # end
 end
