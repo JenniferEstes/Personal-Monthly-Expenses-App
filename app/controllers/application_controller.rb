c68a27ee020cdf23dcfd36b42ee388d7e66b9e40
Jennifer <jenndestes@gmail.com>
2021-03-27 21:49:02 -0500

    Deleted dotenv gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,5 @@ class ApplicationController < Sinatra::Base
     # set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, ENV['SESSION SECRET']
-  end
+    set :session_secret, "secret"
 end
