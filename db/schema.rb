6bb9ed458436b94f8857911dbe408d2702e4177e
Jennifer <jenndestes@gmail.com>
2021-04-04 00:15:52 -0500

    Added id's to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_185603) do
+ActiveRecord::Schema.define(version: 2021_04_04_032148) do
 
   create_table "expenses", force: :cascade do |t|
     t.date "date"
     t.string "description"
-    t.float "amount"
+    t.decimal "amount"
     t.integer "user_id"
   end
 
